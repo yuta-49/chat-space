b25eb30a4cc97ff086f5de741278b6ccccb78ca4
yuta-49 <yumon.f2549@gmail.com>
2019-10-08 16:42:50 +0900

    comit initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :confiqure_permited_parameters, if: :devise_controller?
+
+  def confiqure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
