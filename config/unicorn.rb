013caa0c114b03455749a1f8dd5fcd670a99505b
yuta-49 <yumon.f2549@gmail.com>
2019-10-17 19:51:33 +0900

    変更しました。

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,13 +16,13 @@ pid "#{app_path}/tmp/pids/unicorn.pid"
 #ポート番号を指定
 # listen 3000
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+# listen "#{app_path}/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# #エラーのログを記録するファイルを指定
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# #通常のログを記録するファイルを指定
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
